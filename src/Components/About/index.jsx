607da2b393cee4ce647baf5f02c115a3bb3d2232
Wilson Dev <wilson.hch@hynkor.com>
2023-10-19 09:48:06 -0500

    Modificaciones en About y NavBar

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -5,7 +5,9 @@ const About = () => {
   return (
     <section id="about" className=" dark:text-white lg:pl-32 lg:pr-32 flex justify-center items-center m-6 ">
       <div className='lg:border border-indigo-400 lg:shadow-lg lg:flex justify-evenly items-center p-4 lg:rounded-md '>
-        <img className='w-96 h-96 border-2 border-dotted border-indigo-400 shadow-xl rounded-lg' src="https://images.unsplash.com/photo-1534665482403-a909d0d97c67?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Imgen sobre mí" />
+        <img 
+          className='lg:w-96 lg:h-96 border-2 border-dotted border-indigo-400 shadow-xl rounded-lg' 
+          src="https://images.unsplash.com/photo-1534665482403-a909d0d97c67?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Imgen sobre mí" />
 
         <div className="  lg:p-16  ">
           <h2 className=" font-bold text-2xl mb-6 mt-6 ">About Myself</h2>
